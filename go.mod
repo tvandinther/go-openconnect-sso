1cc753c92a6f5ce269447616ce0bc544dbdd195b
Tom van Dinther <39470469+tvandinther@users.noreply.github.com>
2025-05-13 11:48:51 +0200

    remove go-kit and use slog
    Signed-off-by: Tom van Dinther <39470469+tvandinther@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/rustycl0ck/go-openconnect-sso
 
 go 1.16
 
-require (
-	github.com/go-kit/kit v0.10.0
-	github.com/mxschmitt/playwright-go v0.1100.1-0.20210430103100-de638a4c485a
-)
+require github.com/mxschmitt/playwright-go v0.1100.1-0.20210430103100-de638a4c485a
--- main.go
+++ main.go
@@ -5,13 +5,11 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
 	"os"
 	"strings"
 	"time"
-
-	"github.com/go-kit/kit/log"
-	"github.com/go-kit/kit/log/level"
 
 	"github.com/mxschmitt/playwright-go"
 	"github.com/rustycl0ck/go-openconnect-sso/config"
@@ -20,58 +18,40 @@ import (
 var (
 	server    = flag.String("server", "", "the OpenConnect VPN server address")
 	ocFile    = flag.String("config", "", "where the OpenConnect config file will be saved")
-	logFormat = flag.String("log-format", "logfmt", "log format (json or logfmt)")
+	logFormat = flag.String("log-format", "text", "log format (json or text)")
 	logLevel  = flag.String("log-level", "info", "log level [WARNING: 'debug' level will print openconnect login cookie to the console] (info, warn, error, debug, none)")
 )
 
-func main() {
-	var logger log.Logger
+var log *slog.Logger
 
+func main() {
 	flag.Parse()
 
-	if *logFormat == "json" {
-		logger = log.NewJSONLogger(log.NewSyncWriter(os.Stderr))
-	} else {
-		logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
-	}
-
-	switch *logLevel {
-	case "info":
-		logger = level.NewFilter(logger, level.AllowInfo())
-	case "warn":
-		logger = level.NewFilter(logger, level.AllowWarn())
-	case "error":
-		logger = level.NewFilter(logger, level.AllowError())
-	case "debug":
-		logger = level.NewFilter(logger, level.AllowDebug())
-	case "none":
-		logger = level.NewFilter(logger, level.AllowNone())
-	}
-
-	logger = log.With(logger, "ts", log.DefaultTimestampUTC, "caller", log.DefaultCaller)
+	log = setupLogger(*logFormat, *logLevel)
+	log.Info("Logger initialized")
 
 	pw, err := playwright.Run()
 	if err != nil {
-		level.Error(logger).Log("msg", "could not launch playwright", "err", err)
+		log.Error("could not launch playwright", "err", err)
 	}
 	browser, err := pw.Firefox.Launch(playwright.BrowserTypeLaunchOptions{
 		Headless: playwright.Bool(false),
 	})
 	if err != nil {
-		level.Error(logger).Log("msg", "could not launch Firefox", "err", err)
+		log.Error("could not launch Firefox", "err", err)
 	}
 	context, err := browser.NewContext()
 	if err != nil {
-		level.Error(logger).Log("msg", "could not create context", "err", err)
+		log.Error("could not create context", "err", err)
 	}
 	page, err := context.NewPage()
 	if err != nil {
-		level.Error(logger).Log("msg", "could not create page", "err", err)
+		log.Error("could not create page", "err", err)
 	}
 
-	initResp, targetVPNServer := initializationStage(logger, *server)
+	initResp, targetVPNServer := initializationStage(*server)
 
-	level.Info(logger).Log("msg", "waiting to detect successful authentication token cookie on the browser")
+	log.Info("waiting to detect successful authentication token cookie on the browser")
 	page.Goto(initResp.LoginURL)
 
 	var tokenCookie playwright.NetworkCookie
@@ -80,13 +60,13 @@ func main() {
 		foundCookie := false
 		cookies, err := context.Cookies()
 		if err != nil {
-			level.Error(logger).Log("msg", "could not get cookies from browser context", "err", err)
+			log.Error("could not get cookies from browser context", "err", err)
 		}
 
 		for _, cookie := range cookies {
 			if cookie.Name == initResp.TokenCookieName {
 				tokenCookie = *cookie
-				level.Info(logger).Log("msg", "received successful authentication token cookie from browser")
+				log.Info("received successful authentication token cookie from browser")
 				foundCookie = true
 				break
 			}
@@ -98,19 +78,59 @@ func main() {
 		time.Sleep(10 * time.Millisecond)
 	}
 
-	finalResp := finalizationStage(logger, targetVPNServer, tokenCookie.Value, initResp.Opaque.Value)
-	level.Info(logger).Log("msg", "received openconnect server fingerprint and connection cookie successfully")
+	finalResp := finalizationStage(targetVPNServer, tokenCookie.Value, initResp.Opaque.Value)
+	log.Info("received openconnect server fingerprint and connection cookie successfully")
 
-	writeOCConfig(logger, finalResp.Cookie, finalResp.Fingerprint, targetVPNServer, *ocFile)
+	writeOCConfig(finalResp.Cookie, finalResp.Fingerprint, targetVPNServer, *ocFile)
 }
 
-func initializationStage(logger log.Logger, url string) (config.InitializationResponse, string) {
-	logger = log.With(logger, "stage", "initialization")
+func setupLogger(format, level string) *slog.Logger {
+	var handler slog.Handler
+
+	var slogLevel slog.Level
+	switch level {
+	case "debug":
+		slogLevel = slog.LevelDebug
+	case "info":
+		slogLevel = slog.LevelInfo
+	case "warn":
+		slogLevel = slog.LevelWarn
+	case "error":
+		slogLevel = slog.LevelError
+	case "none":
+		// Custom "none" level: set to a high level to suppress logs
+		slogLevel = slog.Level(100)
+	default:
+		slogLevel = slog.LevelInfo
+	}
+
+	handlerOpts := &slog.HandlerOptions{
+		Level: slogLevel,
+	}
+
+	switch format {
+	case "json":
+		handler = slog.NewJSONHandler(os.Stderr, handlerOpts)
+	case "text":
+	default:
+		handler = slog.NewTextHandler(os.Stderr, handlerOpts)
+	}
+
+	// Add timestamp and source info
+	logger := slog.New(handler.WithAttrs([]slog.Attr{
+		slog.String("ts", "utc"),
+	}))
+
+	return logger
+}
+
+func initializationStage(url string) (config.InitializationResponse, string) {
+	log = log.With("stage", "initialization")
 
 	// Get the final redirect-url from the initial server
 	resp, err := http.Get(url)
 	if err != nil {
-		level.Error(logger).Log("msg", "Failed to get url", "url", url, "err", err)
+		log.Error("Failed to get url", "url", url, "err", err)
 		os.Exit(1)
 	}
 	targetVPNServer := resp.Request.URL.String()
@@ -127,24 +147,24 @@ func initializationStage(logger log.Logger, url string) (config.InitializationRe
       </capabilities>
     </config-auth>
 	`, targetVPNServer)
-	level.Debug(logger).Log("targetVPNServer", targetVPNServer)
+	log.Debug("configuring VPN request", "targetVPNServer", targetVPNServer)
 
 	var result config.InitializationResponse
-	body := makePostReq(logger, xmlPayload, targetVPNServer)
+	body := makePostReq(xmlPayload, targetVPNServer)
 
-	level.Debug(logger).Log("msg", "received response from server", "body", string(body))
+	log.Debug("received response from server", "body", string(body))
 
 	if err := xml.Unmarshal(body, &result); err != nil {
-		level.Error(logger).Log("msg", "failed to unmarshal the received response body to XML", "err", err, "body", string(body))
+		log.Error("failed to unmarshal the received response body to XML", "err", err, "body", string(body))
 		os.Exit(1)
 	}
 
-	level.Info(logger).Log("msg", "unmarshalled init response", "loginURL", result.LoginURL, "loginFinalURL", result.LoginFinalURL, "tokenCookieName", result.TokenCookieName, "opaque", result.Opaque.Value)
+	log.Info("unmarshalled init response", "loginURL", result.LoginURL, "loginFinalURL", result.LoginFinalURL, "tokenCookieName", result.TokenCookieName, "opaque", result.Opaque.Value)
 	return result, targetVPNServer
 }
 
-func finalizationStage(logger log.Logger, vpnServer string, token string, configHash string) config.FinalizationResponse {
-	logger = log.With(logger, "stage", "finalization")
+func finalizationStage(vpnServer string, token string, configHash string) config.FinalizationResponse {
+	log = log.With("stage", "finalization")
 
 	xmlPayload := fmt.Sprintf(`
     <config-auth client="vpn" type="auth-reply" aggregate-auth-version="2">
@@ -160,24 +180,24 @@ func finalizationStage(logger log.Logger, vpnServer string, token string, config
   `, configHash, token)
 
 	var result config.FinalizationResponse
-	body := makePostReq(logger, xmlPayload, vpnServer)
+	body := makePostReq(xmlPayload, vpnServer)
 
-	level.Debug(logger).Log("msg", "received response from server", "body", string(body))
+	log.Debug("received response from server", "body", string(body))
 
 	if err := xml.Unmarshal(body, &result); err != nil {
-		level.Error(logger).Log("msg", "failed to unmarshal the received response body to XML", "err", err, "body", string(body))
+		log.Error("failed to unmarshal the received response body to XML", "err", err, "body", string(body))
 		os.Exit(1)
 	}
 
-	level.Debug(logger).Log("msg", "unmarshalled final response", "cookie", result.Cookie, "fingerprint", result.Fingerprint)
+	log.Debug("unmarshalled final response", "cookie", result.Cookie, "fingerprint", result.Fingerprint)
 	return result
 }
 
-func makePostReq(logger log.Logger, xmlPayload, server string) []byte {
+func makePostReq(xmlPayload, server string) []byte {
 
 	req, err := http.NewRequest("POST", server, strings.NewReader(xmlPayload))
 	if err != nil {
-		level.Error(logger).Log("msg", "Failed to create http request", "err", err)
+		log.Error("Failed to create http request", "err", err)
 		os.Exit(1)
 	}
 	headers := map[string]string{
@@ -195,21 +215,21 @@ func makePostReq(logger log.Logger, xmlPayload, server string) []byte {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		level.Error(logger).Log("msg", "failed to POST request to the server", "server", req.URL.String(), "err", err)
+		log.Error("failed to POST request to the server", "server", req.URL.String(), "err", err)
 		os.Exit(1)
 	}
 	body, _ := io.ReadAll(resp.Body)
-	level.Info(logger).Log("msg", "successfullly received response from server", "url", resp.Request.URL.String())
-	level.Debug(logger).Log("msg", "received response", "body", string(body), "url", resp.Request.URL.String())
+	log.Info("successfullly received response from server", "url", resp.Request.URL.String())
+	log.Debug("received response", "body", string(body), "url", resp.Request.URL.String())
 
 	return body
 }
 
-func writeOCConfig(logger log.Logger, cookie, fingerprint, server, ocFile string) {
+func writeOCConfig(cookie, fingerprint, server, ocFile string) {
 	content := fmt.Sprintf("cookie=%s\nservercert=%s\n# host=%s\n", cookie, fingerprint, server)
 	if err := os.WriteFile(ocFile, []byte(content), 0600); err != nil {
-		level.Error(logger).Log("msg", "failed to write authentication details to file", "file", ocFile, "err", err)
+		log.Error("failed to write authentication details to file", "file", ocFile, "err", err)
 		os.Exit(1)
 	}
-	level.Info(logger).Log("msg", "successfully written authentication details to file", "file", ocFile)
+	log.Info("successfully written authentication details to file", "file", ocFile)
 }
